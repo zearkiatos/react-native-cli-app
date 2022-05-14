02efd9d5e7bf699fc6f4e5980401db4c56503ac7
zearkiatos <caprilespe@outlook.com>
2022-05-13 20:14:34 -0400

    Add a hello world

---
+++ app/src/App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Hello World! 👋🌎</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'black',
+  },
+});
+
+export default App;
